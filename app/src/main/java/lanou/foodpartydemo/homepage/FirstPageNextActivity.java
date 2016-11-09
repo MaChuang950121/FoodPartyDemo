e7473d62225f783aa396ac60a21f555cfe5b380d
马闯 <674195845@qq.com>
2016-11-09 20:51:14 +0800

    no message

---
+++ app/src/main/java/lanou/foodpartydemo/homepage/FirstPageNextActivity.java
@@ -0,0 +1,7 @@
+package lanou.foodpartydemo.homepage;
+
+/**
+ * Created by dllo on 16/11/8.
+ */
+public class FirstPageNextActivity {
+}
---
+++ app/src/main/java/lanou/foodpartydemo/library/SearchActivity.java
@@ -0,0 +1,7 @@
+package lanou.foodpartydemo.library;
+
+/**
+ * Created by dllo on 16/11/9.
+ */
+public class SearchActivity {
+}
